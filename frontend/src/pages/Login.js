7daf013146fb5c9640e6c41099475412e20c38e0
LIKITH SAI REDDY SYAMALA <110295327+syamala-likith@users.noreply.github.com>
2025-06-06 00:32:18 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Login.css"; // 👈 Add this line
+import "./Login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -10,7 +10,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://launchmytech.onrender.com/api/auth/login", {
         email,
         password,
       });
